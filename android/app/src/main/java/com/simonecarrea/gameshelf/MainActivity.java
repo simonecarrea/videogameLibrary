09f74b712c161cef2ce83088d4027e6f5a14c375
simonecarrea <carre.7@hotmail.it>
2026-08-31 17:52:44 +0200

    Load bundled GameShelf assets in Android app

--- android/app/src/main/java/com/simonecarrea/gameshelf/MainActivity.java
+++ android/app/src/main/java/com/simonecarrea/gameshelf/MainActivity.java
@@ -3,43 +3,46 @@ package com.simonecarrea.gameshelf;
 import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.WindowInsets;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
+  private static final String LOCAL_APP_URL = "file:///android_asset/index.html";
   private WebView webView;
 
   @Override public void onCreate(Bundle savedInstanceState){
     super.onCreate(savedInstanceState);
-    webView=new WebView(this);
+
+    webView = new WebView(this);
     webView.setBackgroundColor(0xFF080A0F);
 
-    WebSettings s=webView.getSettings();
-    s.setJavaScriptEnabled(true);
-    s.setDomStorageEnabled(true);
-    s.setDatabaseEnabled(true);
-    s.setMediaPlaybackRequiresUserGesture(false);
+    WebSettings settings = webView.getSettings();
+    settings.setJavaScriptEnabled(true);
+    settings.setDomStorageEnabled(true);
+    settings.setDatabaseEnabled(true);
+    settings.setMediaPlaybackRequiresUserGesture(false);
+    settings.setAllowFileAccess(true);
+    settings.setAllowContentAccess(false);
 
     webView.setWebViewClient(new WebViewClient());
 
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-      webView.setOnApplyWindowInsetsListener((v, insets) -> {
+      webView.setOnApplyWindowInsetsListener((view, insets) -> {
         int top = insets.getSystemWindowInsetTop();
         int bottom = insets.getSystemWindowInsetBottom();
-        v.setPadding(0, top, 0, bottom);
+        view.setPadding(0, top, 0, bottom);
         return insets;
       });
       webView.requestApplyInsets();
     }
 
-    webView.loadUrl("https://simonecarrea.github.io/videogameLibrary/");
+    webView.loadUrl(LOCAL_APP_URL);
     setContentView(webView);
   }
 
   @Override public void onBackPressed(){
-    if(webView.canGoBack()) webView.goBack();
+    if (webView != null && webView.canGoBack()) webView.goBack();
     else super.onBackPressed();
   }
 }
